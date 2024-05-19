a924cac22cd0a26ae584a0c4c5d10d557d5037a9
PeterVillanueva <peterdonniel4444@gmail.com>
2024-05-19 21:33:01 +0800

    Final Fixes for Ui

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => (
     <div className="absolute w-[50%] inset-0
      gradient-01"
     />
-    <div className={`${styles.innerwidth} mx-auto flex justify-between gap-8`}>
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
         src="search.svg"
         alt="search"
